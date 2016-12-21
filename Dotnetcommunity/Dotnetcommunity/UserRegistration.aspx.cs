f08220f32940b8d73af58fcf66e9ede7b99f130f
Prudhvi Raj Somepalli <somepalliprudhvi@gmail.com>
2016-12-21 10:31:36 -0500

    ddl Update in ddrop down list

--- Dotnetcommunity/Dotnetcommunity/UserRegistration.aspx.cs
+++ Dotnetcommunity/Dotnetcommunity/UserRegistration.aspx.cs
@@ -12,18 +12,43 @@ namespace Dotnetcommunity
 {
     public partial class UserRegistration : System.Web.UI.Page
     {
+        SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["cs"].ToString());
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            Security_Question();
         }
+
+        protected void Security_Question()
+        {
+            try
+            {
+
+                conn.Open();
+                SqlCommand cmd = new SqlCommand("select * FROM ddlQuestion",conn);
+                SqlDataAdapter Da = new SqlDataAdapter(cmd);
+                DataSet Ds = new DataSet();
+                Da.Fill(Ds);
+                ddlQuestion.DataTextField = Ds.Tables[0].Columns["Question"].ToString();
+                ddlQuestion.DataValueField = Ds.Tables[0].Columns["QuestionId"].ToString();
+                ddlQuestion.DataSource = Ds.Tables[0];
+                ddlQuestion.DataBind();
+            }
+            catch
+            {
+                Console.WriteLine("this is error ");
+
+            }
+        }
+
 
         protected void btnSubmit_Click(object sender, EventArgs e)
         {
             try
             {
-                SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["cs"].ToString());
+                
                 conn.Open();
                 SqlCommand cmd = new SqlCommand();
+
 
             }
             catch
